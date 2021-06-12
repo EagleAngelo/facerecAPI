0dfecdb1c5193d4c0598838a1b4182cbe9eb4423
EagleAngelo <EagleAngelo@users.noreply.github.com>
2021-06-11 19:19:56 -0600

    register

--- controllers/register.js
+++ controllers/register.js
@@ -7,32 +7,30 @@ const handleRegister = (req, res, db, bcrypt) => {
     }
 
     bcrypt.genSalt(10, function (err, salt) {
-        bcrypt
-            .hash(password, salt, function (err, hash) {
-                db.transaction((trx) => {
-                    trx.insert({
-                        hash: hash,
-                        email: email,
+        bcrypt.hash(password, salt, function (err, hash) {
+            db.transaction((trx) => {
+                trx.insert({
+                    hash: hash,
+                    email: email,
+                })
+                    .into("login")
+                    .returning("email")
+                    .then((loginEmail) => {
+                        return trx("users")
+                            .returning("*")
+                            .insert({
+                                email: loginEmail[0],
+                                name: name,
+                                joined: new Date(),
+                            })
+                            .then((user) => {
+                                res.json(user[0]);
+                            });
                     })
-                        .into("login")
-                        .returning("email")
-                        .then((loginEmail) => {
-                            return trx("users")
-                                .returning("*")
-                                .insert({
-                                    email: loginEmail[0],
-                                    name: name,
-                                    joined: new Date(),
-                                })
-                                .then((user) => {
-                                    res.json(user[0]);
-                                });
-                        })
-                        .then(trx.commit)
-                        .catch(trx.rollback);
-                }).catch((err) => res.status(400).json("unable to register"));
-            })
-            .catch((err) => res.status(400).json("invalid pass"));
+                    .then(trx.commit)
+                    .catch(trx.rollback);
+            }).catch((err) => res.status(400).json("unable to register"));
+        });
     });
 };
 
