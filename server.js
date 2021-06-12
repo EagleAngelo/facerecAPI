89c693ead442ac53836503d1f4253dd744f794b8
EagleAngelo <EagleAngelo@users.noreply.github.com>
2021-06-11 18:29:17 -0600

    cors

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.all("*", function (req, res, next) {
+    var origin = req.get("origin");
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
 const db = knex({
     client: "pg",
     connection: {
